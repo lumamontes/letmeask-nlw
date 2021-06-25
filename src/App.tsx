fa2aab0af03de8702efeac52783f3d04467f4a02
Luma Montes <lumagoesmontes@gmail.com>
2021-06-24 23:49:11 -0300

    adicionado navegaçao entre paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import {NewRoom} from './pages/NewRoom'
+import {Home} from './pages/Home'
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+      
+      <NewRoom />
+    
+    </BrowserRouter>
     );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
+import { useHistory } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 import '../styles/auth.scss';
 export function Home() {
+    const history = useHistory();
+
+    function navigateToNewRoom() {
+        history.push('/rooms/new')
+    }
     return (
         <div id="page-auth">
             <aside>
@@ -15,7 +21,10 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Imagem de logo LetmeAsk" />
-                    <button className="create-room">
+                    <button 
+                    className="create-room"
+                    onClick={navigateToNewRoom}
+                    >
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
